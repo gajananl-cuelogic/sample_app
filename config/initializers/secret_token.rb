758516a485f413401f8299fb98d8b56200558446
Gajanan londhe <gajanan.londhe@cuelogic.co.in>
2017-06-16 17:41:37 +0530

    initialise the master

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '7c3c096859b7a9addd666b91374d8206017ac468259cffc845b991e53a1d192982f885057cd31e900509f027e047653f67c0c48b5fbf93454cf7df68074f5773'
