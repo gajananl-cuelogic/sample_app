c8b519db009fc225273a35515382f3ff83fb153a
Gajanan londhe <gajanan.londhe@cuelogic.co.in>
2017-06-28 16:24:33 +0530

    updating-users-updato 384

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -1,41 +1,4 @@
-=begin require 'spec_helper'
-	describe "Authentication" do
-	subject { page }
-							describe "signin page" do
-							before { visit signin_path }
-								it { should have_selector('h1', text: 'Sign in') }
-								it { should have_selector('title', text: 'Sign in') }
-							end
-
-				
-         describe "signin" do
-	     before { visit signin_path }
-					      
-                    describe "with invalid information" do
-					before { click_button "Sign in" }
-					it { should have_selector('title', text: 'Sign in') }
-					it { should have_selector('div.alert.alert-error', text: 'Invalid') }
-					end
-
-
-
- 
-				    describe "with valid information" do
-				     let(:user) { FactoryGirl.create(:user) }
-								before do
-								fill_in "Email",with: user.email		
-								fill_in "Password", with: user.password
-								click_button "Sign in"
-								end
-							it {should have_selector('title', text: user.name) }
-							it {should have_link('Profile', href: user_path(user)) }
-							it {should have_link('Sign out', href: signout_path) }
-							it {should_not have_link('Sign in', href: signin_path) }
-					 end
-
-			end 		
-    end
-=end    
+  
 
 require 'spec_helper'
 
@@ -52,8 +15,7 @@ describe "Authentication" do
 	before { visit signin_path }
 
 
-		describe "with invalid information" do
-			#before { signin_path  "Sign in" } 
+		describe "with invalid information" do			
 			before { click_button "Sign in" }
 			it { should have_selector('title', text: 'Sign in') }
 			it { should have_selector('div.alert.alert-error', text: 'Invalid') }
@@ -66,6 +28,8 @@ describe "Authentication" do
 	
 	    describe "with valid information" do
 	    	let(:user){FactoryGirl.create(:user)}    	
+            
+           
 
 			before do
 			 fill_in "Email",with:user.email
@@ -74,7 +38,8 @@ describe "Authentication" do
 			end
 
 			it { should have_selector('title', text: user.name) }
-			it { should have_link('Profile', href: user_path(user)) }		
+			it { should have_link('Profile', href: user_path(user)) }
+			it { should have_link('Settings', href: edit_user_path(user)) }		
 			it { should have_link('Sign out', href: signout_path) }
 			it { should_not have_link('Sign in', href: signin_path) }	
 
