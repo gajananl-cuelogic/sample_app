edc2ca3d75a26719256c11879029758bb0e785a4
Gajanan londhe <gajanan.londhe@cuelogic.co.in>
2017-06-29 19:44:18 +0530

    runnig all test cases upto page 389

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,45 @@
 class UsersController < ApplicationController
-   def show
-	@user = User.find(params[:id])
-	end
-	def new
-		@user = User.new
-	end
-   
-    def create
-	@user = User.new(params[:user])
-		if @user.save
-			sign_in @user
-			flash[:success] = "Welcome to the Sample App!"
-			redirect_to @user
-		else
-		render 'new'
-		end
-	end
-
-	def edit
+#=begin
+        before_filter :signed_in_user, only: [:edit, :update]
+#=end	
+	    def show
 		@user = User.find(params[:id])
-	end
-
-	def update
-	@user = User.find(params[:id])
-		if @user.update_attributes(params[:user])
-			flash[:success] = "Profile updated"
-			sign_in @user
-			redirect_to @user
-		# Handle a successful update.
-		else
-		render 'edit'
 		end
-	end
+		def new
+			@user = User.new
+		end
+   
+	    def create
+		@user = User.new(params[:user])
+				if @user.save
+					sign_in @user
+					flash[:success] = "Welcome to the Sample App!"
+					redirect_to @user
+				else
+				render 'new'
+				end
+		end
 
+		def edit
+			@user = User.find(params[:id])
+		end
+
+		def update
+		@user = User.find(params[:id])
+				if @user.update_attributes(params[:user])
+					flash[:success] = "Profile updated"
+					sign_in @user
+					redirect_to @user
+				# Handle a successful update.
+				else
+				render 'edit'
+				end
+		end
+#=begin	
+		private
+		
+			def signed_in_user
+				redirect_to signin_path, notice: "Please sign in." unless signed_in?
+			end
+#=end
 end
